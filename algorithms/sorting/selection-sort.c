14c98e35fdf8f569e870b7269041b27517708620
albannakhaled <albannakhalil90@gmail.com>
2023-07-14 12:15:05 +0300

    merge sort , selection sort

---
+++ algorithms/sorting/selection-sort.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+// Function to swap two elements
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+// Function to perform Selection Sort
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+
+        // Find the index of the minimum element in the unsorted part of the array
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap the minimum element with the first element of the unsorted part
+        swap(&arr[i], &arr[minIndex]);
+    }
+}
+
+// Function to print the elements of the array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+// Main function to test the Selection Sort algorithm
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array: ");
+    printArray(arr, n);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array: ");
+    printArray(arr, n);
+
+    return 0;
+}
